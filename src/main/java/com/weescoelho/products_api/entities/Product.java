bb3648159cda120da2ec669051f7797120f63693
Weslley Coelho <wes.coelho18@gmail.com>
2024-04-25 23:17:37 -0300

    implementação do relacionamento many-to-many na entidade produtos e categoria

--- src/main/java/com/weescoelho/products_api/entities/Product.java
+++ src/main/java/com/weescoelho/products_api/entities/Product.java
@@ -8,6 +8,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
@@ -23,7 +26,9 @@ public class Product implements Serializable {
   private Double price;
   private String imgUrl;
 
-  @Transient // JPA ignora e não irá ser interpretado
+  // @Transient // JPA ignora e não irá ser interpretado
+  @ManyToMany
+  @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
   private Set<Category> categories = new HashSet<>();
 
   public Product() {
