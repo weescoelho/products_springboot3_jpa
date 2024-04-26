192b0e16297068751dbeff1400b9ecff05195e2f
Weslley Coelho <wes.coelho18@gmail.com>
2024-04-25 22:18:05 -0300

    Implementação da entidade de Order

--- src/main/java/com/weescoelho/products_api/entities/User.java
+++ src/main/java/com/weescoelho/products_api/entities/User.java
@@ -1,11 +1,14 @@
 package com.weescoelho.products_api.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,6 +21,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -68,6 +74,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
