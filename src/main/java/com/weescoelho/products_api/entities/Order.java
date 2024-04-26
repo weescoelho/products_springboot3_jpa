d79ff8983ccf5aafb08910db4adfb47024ee1423
Weslley Coelho <wes.coelho18@gmail.com>
2024-04-25 22:45:53 -0300

    Implementação do OrderStatus Enum

--- src/main/java/com/weescoelho/products_api/entities/Order.java
+++ src/main/java/com/weescoelho/products_api/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.weescoelho.products_api.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -24,6 +25,8 @@ public class Order implements Serializable {
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
   private Instant moment;
 
+  private Integer orderStatus;
+
   @ManyToOne
   @JoinColumn(name = "client_id")
   private User client;
@@ -31,9 +34,10 @@ public class Order implements Serializable {
   public Order() {
   }
 
-  public Order(Long id, Instant moment, User client) {
+  public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
     this.id = id;
     this.moment = moment;
+    setOrderStatus(orderStatus);
     this.client = client;
   }
 
@@ -59,6 +63,15 @@ public class Order implements Serializable {
 
   public void setClient(User client) {
     this.client = client;
+  }
+
+  public OrderStatus getOrderStatus() {
+    return OrderStatus.valueOf(orderStatus);
+  }
+
+  public void setOrderStatus(OrderStatus orderStatus) {
+    if (orderStatus != null)
+      this.orderStatus = orderStatus.getCode();
   }
 
   @Override
